d8ec9df45e62cfa879f44a048ec8a9c7ad323589
crashtack <crashtack@gmail.com>
2016-07-15 10:44:47 -0700

    saving some syntax examples

---
+++ pythonSyntax.py
@@ -0,0 +1,17 @@
+a = True
+b = False
+my_int = 7
+my_float = 1.23
+my_bool = True
+if a == b:
+    print("hello")
+else:
+    print("a is not equal to b")
+
+# Exponentiation
+eggs = 10**2
+print(eggs)
+
+# Modulo returns the remainder
+remander = 5 % 4
+print(remander)
