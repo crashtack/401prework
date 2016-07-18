cb11410f781fb49a112334ab3a2626b1fc58fc63
crashtack <crashtack@gmail.com>
2016-07-18 12:43:46 -0700

    leasson code

---
+++ strings.py
@@ -0,0 +1,20 @@
+parrot = "Norwegian Blue"
+print(len(parrot))
+print(parrot.lower())
+print(parrot.upper())
+print(parrot)
+
+pi = 3.14
+print(str(pi))
+
+string_1 = "Camelot"
+string_2 = "place"
+
+print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
+
+name = input("What is your name?")
+quest = input("What is your quest?")
+color = input("What is your favorite color?")
+
+print("Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color))
