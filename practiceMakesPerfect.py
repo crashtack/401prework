79e8effabc84fa9f3a114cf0e2735144cc21a1e5
crashtack <crashtack@gmail.com>
2016-08-03 10:16:41 -0700

    after scrabble score

--- practiceMakesPerfect.py
+++ practiceMakesPerfect.py
@@ -57,3 +57,27 @@ def reverse(text):
     return output
 
 # print(reverse("123456%^&*"))
+
+def anti_vowel(text):
+    vowels = 'aeiouAEIOU'
+    for c in vowels:
+        text = ''.join(text.split(c))
+        # print(text)
+    return text
+
+# print(anti_vowel('Hello World'))
+
+def scrabble_score(word):
+    score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+         "x": 8, "z": 10}
+    total = 0
+    word = word.lower()
+    for c in word:
+        total = total + score['{}'.format(c)]
+    #print(total)
+    return total
+
+print('total = {}'.format(scrabble_score("Helix")))
