6ee1427a70bdb0bd753e914883985af807a917d3
crashtack <crashtack@gmail.com>
2016-08-04 23:21:58 -0700

    ExamStatistics.py

--- ExamStatistics.py
+++ ExamStatistics.py
@@ -13,10 +13,24 @@ def grades_sum(scores):
         sum = sum + score
     return sum
 
-# print('Sum: {}'.format(grades_sum(grades)))
+print('Sum: {}'.format(grades_sum(grades)))
 
 def grades_average(grades):
     average = grades_sum(grades)/float(len(grades))
     return average
 
 print('Average: {0:.2f}'.format(grades_average(grades)))
+
+def grades_variance(scores):
+    average = grades_average(scores)
+    variance = 0
+    for score in scores:
+        variance = variance + ((average - score) ** 2)
+    return variance / len(scores)
+
+print('Variance: {0:.2f}'.format(grades_variance(grades)))
+
+def grades_std_deviation(variance):
+    return grades_variance(grades) ** 0.5
+
+print('Standard Deviation: {0:.2f}'.format(grades_std_deviation(grades_variance(grades))))
